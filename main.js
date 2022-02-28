02f99745793f89639d7635d852ec3fdb15d59d1b
jiitra <74521649+jiitra@users.noreply.github.com>
2022-02-28 14:54:48 -0500

    :star: Added normalize & removed "other work" from header

--- main.js
+++ main.js
@@ -6,19 +6,26 @@ const buttonElement = document.querySelector(".js-color-mode__btn");
 buttonElement.addEventListener('click', function () {
 
     //if (colorMode == "light") {
+    if (document.documentElement.getAttribute("color-mode") == "light") {
         document.documentElement.setAttribute("color-mode", "dark");
 
         // Sets the user's preference in local storage
         localStorage.setItem("color-mode", "dark");
-    //}
-});
+    }
+    else{
+        document.documentElement.setAttribute("color-mode", "light");
+        localStorage.setItem("color-mode", "light");
+    }
+    });
 
 if (
     /* This condition checks whether the user has set a site preference for dark mode OR a OS-level preference for Dark Mode AND no site preference */
     localStorage.getItem('color-mode') === 'dark' ||
     (window.matchMedia('(prefers-color-scheme: dark)').matches &&
-     !localStorage.getItem('color-mode'))
+        !localStorage.getItem('color-mode'))
 ) {
     // if true, set the site to Dark Mode
-    document.documentElement.setAttribute('color-mode', 'dark')
+    document.documentElement.setAttribute('color-mode', 'dark');
+} else{
+    document.documentElement.setAttribute('color-mode', 'light');
 }
